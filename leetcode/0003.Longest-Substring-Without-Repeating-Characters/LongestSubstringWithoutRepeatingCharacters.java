0f066f43bfe06cdbf30e02264d20d9d744100f02
changqingyeh <yecq1998@outlook.com>
2021-03-07 21:49:26 +0800

    Add .gitignore

--- leetcode/0003.Longest-Substring-Without-Repeating-Characters/LongestSubstringWithoutRepeatingCharacters.java
+++ leetcode/0003.Longest-Substring-Without-Repeating-Characters/LongestSubstringWithoutRepeatingCharacters.java
@@ -1,42 +1,26 @@
-import java.util.HashSet;
-import java.util.Set;
+import java.util.HashMap;
+import java.util.Map;
 
 public class LongestSubstringWithoutRepeatingCharacters {
 
 	public int lengthOfLongestSubstring(String s) {
-		char[] array = s.toCharArray();
-		Set<Character> set = new HashSet<>();
-		int length = array.length;
+		if (s == null || s.length() == 0) {
+			return 0;
+		}
+		if (s.length() == 1) {
+			return 1;
+		}
 		int max = 0;
-		int cur = -1;
-		int i;
-		for (i = 0; i < length; i++) {
-			if (i != 0) {
-				set.remove(array[i - 1]);
+		int left = 0;
+		Map<Character, Integer> map = new HashMap<>();
+		for (int i = 0; i < s.length(); i++) {
+			char c = s.charAt(i);
+			if (map.containsKey(c)) {
+				left = Math.max(left, map.get(c) + 1);
 			}
-			while (cur + 1 < length){
-				if(!set.add(array[cur + 1])){
-					break;
-				}
-				cur++;
-			}
-			max = Math.max(max, cur - i + 1);
+			map.put(c, i);
+			max = Math.max(max, i - left + 1);
 		}
 		return max;
-	}
-
-	public static void main(String[] args) {
-		LongestSubstringWithoutRepeatingCharacters test = new LongestSubstringWithoutRepeatingCharacters();
-		String case1= "a";
-		int result1 = test.lengthOfLongestSubstring(case1);
-		System.out.println("1: " + result1);
-
-		String case2= "asdfga";
-		int result2 = test.lengthOfLongestSubstring(case2);
-		System.out.println("5: " + result2);
-
-		String case3= "aa";
-		int result3 = test.lengthOfLongestSubstring(case3);
-		System.out.println("1: " + result3);
 	}
 }
