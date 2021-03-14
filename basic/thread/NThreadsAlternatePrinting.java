48c47d46ecb03dbcb0ce888bba7e51cd7c1455a3
changqingyeh <yecq1998@outlook.com>
2021-03-14 23:34:11 +0800

    多线程交替打印

---
+++ basic/thread/NThreadsAlternatePrinting.java
@@ -0,0 +1,55 @@
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class ThreeThreadsAlternatePrinting {
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread[] thread = new Thread[10];
+        for (int i = 0; i < 10; i++){
+            thread[i] = new Thread(new Task(i));
+        }
+        for (int i = 0; i < 10; i++){
+            thread[i].start();
+        }
+        for (int i = 0; i < 10; i++){
+            thread[i].join();
+        }
+    }
+
+    static class Task implements Runnable{
+
+        private static final ReentrantLock lock = new ReentrantLock();
+        private static final Condition cond = lock.newCondition();
+        private static int count;
+        private final int num;
+
+        public Task(int num) {
+            this.num = num;
+        }
+
+
+        @Override
+        public void run() {
+            while(true){
+                lock.lock();
+                if(count >= 30){
+                    lock.unlock();
+                    break;
+                }
+                if(count % 10 == num){
+                    System.out.println("Thread " + num + " ----> " + count);
+                    count++;
+                }else {
+                    try {
+                        cond.await();
+                    } catch (InterruptedException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
+                }
+                cond.signalAll();
+                lock.unlock();
+            }
+        }
+    }
+}
---
+++ basic/thread/TwoThreadsAlternatePrinting.java
@@ -0,0 +1,36 @@
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+
+public class TwoThreadsAlternatePrinting {
+
+    private volatile static int flag = 0;
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread t1 = new Thread() {
+            @Override
+            public void run() {
+                for (int i = 1; i <= 10; i++) {
+                    while (flag == 1) {
+                    }
+                    System.out.println("A");
+                    flag = 1;
+                }
+            }
+        };
+
+        Thread t2 = new Thread() {
+            public void run() {
+                for (int i = 1; i <= 10; i++) {
+                    while (flag == 0) {
+                    }
+                    System.out.println("B");
+                    flag = 0;
+                }
+            };
+        };
+        t1.start();
+        t2.start();
+        t1.join();
+        t2.join();
+    }
+}
