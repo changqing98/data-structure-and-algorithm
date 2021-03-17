8ed7abf21dd802aa3e0986ef59c070782cdae588
changqingyeh <yecq1998@outlook.com>
2021-03-17 22:50:23 +0800

    ))35

---
+++ leetcode/0035.SearchInsertPosition/SearchInsert.java
@@ -0,0 +1,21 @@
+public class SearchInsert {
+    public int searchInsert(int[] nums, int target) {
+        int l = 0;
+        int r = nums.length - 1;
+        while(l < r){
+            int mid = (l + r) / 2;
+            if(nums[mid] == target){
+                return mid;
+            } else if (nums[mid] < target){
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+            }
+        }
+        if(nums[l] < target){
+            return l + 1;
+        } else {
+            return l;
+        }
+    }
+}
