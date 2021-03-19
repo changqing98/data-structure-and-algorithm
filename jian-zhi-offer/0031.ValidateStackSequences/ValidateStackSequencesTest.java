04eb0f5f9a666944b40ee1df0d169ca08fbccf48
changqingyeh <yecq1998@outlook.com>
2021-03-20 00:06:52 +0800

    jian-zhi-offer/0031.ValidateStackSequences

---
+++ jian-zhi-offer/0031.ValidateStackSequences/ValidateStackSequencesTest.java
@@ -0,0 +1,11 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class ValidateStackSequencesTest {
+  @Test
+  public void test() {
+    var inst = new ValidateStackSequences();
+    assertEquals(true, inst.validateStackSequences(new int[] { 1, 2, 3, 4, 5 }, new int[] { 4, 5, 3, 2, 1 }));
+  }
+}
