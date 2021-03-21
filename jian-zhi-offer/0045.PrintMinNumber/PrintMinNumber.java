e9af9e7bc08175beca3452c54dfb9738a1523630
changqingyeh <yecq1998@outlook.com>
2021-03-21 18:03:18 +0800

    :sparkles: 剑指 Offer 33. 二叉搜索树的后序遍历序列

--- jian-zhi-offer/0045.PrintMinNumber/PrintMinNumber.java
+++ jian-zhi-offer/0045.PrintMinNumber/PrintMinNumber.java
@@ -1,17 +1,18 @@
 import java.util.Arrays;
+import java.util.stream.Collectors;
 
 public class PrintMinNumber {
-    public String PrintMinNumber(int [] numbers) {
-        if(numbers.length == 0){
+    public String PrintMinNumber(int[] nums) {
+        if (nums.length == 0) {
             return "0";
         }
-        String[] numStrs = new String[numbers.length];
-        for(int i=0;i<numbers.length;i++){
-            numStrs[i] = numbers[i] + "";
+        String[] numStrs = new String[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            numStrs[i] = nums[i] + "";
         }
-        Arrays.sort(numStrs, (a, b) -> (a+b).compareTo(b+a));
+        Arrays.sort(numStrs, (a, b) -> (a + b).compareTo(b + a));
         StringBuilder result = new StringBuilder();
-        for(int i = 0; i< numStrs.length; i++){
+        for (int i = 0; i < numStrs.length; i++) {
             result.append(numStrs[i]);
         }
         return result.toString();
@@ -19,6 +20,6 @@ public class PrintMinNumber {
 
     public static void main(String[] args) {
         PrintMinNumber test = new PrintMinNumber();
-        System.out.println(test.PrintMinNumber(new int[]{3, 32, 321}));
+        System.out.println(test.PrintMinNumber(new int[] { 3, 32, 321 }));
     }
 }
