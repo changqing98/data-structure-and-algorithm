e6253099e2d65687fd6dc9b3a6a7ec19fda228d9
changqingyeh <yecq1998@outlook.com>
2021-03-31 22:32:57 +0800

    53. 最大子序和

--- leetcode/0053.Maximum-Subarray/MaximumSubarray.java
+++ leetcode/0053.Maximum-Subarray/MaximumSubarray.java
@@ -1,20 +1,15 @@
 public class MaximumSubarray {
-
     public int maxSubArray(int[] nums) {
         int max = nums[0];
         int sum = nums[0];
         for (int i = 1; i < nums.length; i++) {
-            if (sum <= 0){
+            if (sum <= 0) {
                 sum = nums[i];
-            }else {
+            } else {
                 sum += nums[i];
             }
             max = Math.max(sum, max);
         }
         return max;
-    }
-
-    public static void main(String[] args) {
-        
     }
 }
