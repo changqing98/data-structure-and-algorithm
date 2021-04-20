1396e5c7e68d1c6632dc14ee2a087b0179d82446
changqingyeh <yecq1998@outlook.com>
2021-04-20 21:27:07 +0800

    204. 统计质数

---
+++ leetcode/0204.count-primes/CountPrimes.java
@@ -0,0 +1,38 @@
+public class CountPrimes {
+
+    public int countPrimes(int n) {
+        List<Integer> primes = new ArrayList<>();
+        boolean[] tmp = new boolean[n];
+        Arrays.fill(tmp, true);
+        for(int i = 2; i < n; i++){
+            if(tmp[i]){
+                primes.add(i);
+            }
+            for(int j = 0; j < primes.size() && i * primes.get(j) < n; j++){
+                tmp[i * primes.get(j)] = false;
+                if(i % primes.get(j) == 0){
+                    break;
+                }
+            }
+        }
+        return primes.size();
+    }
+
+
+    public int countPrimes2(int n) {
+        int count = 0;
+        for(int i = 2; i < n; i++){
+            count += isPrime(i);
+        }
+        return count;
+    }
+
+    private int isPrime(int n){
+        for(int i = 2; i <= Math.sqrt(n); i++){
+            if(n % i == 0){
+                return 0;
+            }
+        }
+        return 1;
+    }
+}
