caedcf6983916a75325fdf58b4268ed709994b10
changqingyeh <yecq1998@outlook.com>
2021-06-01 22:46:36 +0800

    1744. 你能在你最喜欢的那天吃到你最喜欢的糖果吗？

---
+++ leetcode/1744.can-you-eat-your-favorite-candy-on-your-favorite-day/CanEat.java
@@ -0,0 +1,18 @@
+public class CanEat {
+    public boolean[] canEat(int[] candiesCount, int[][] queries) {
+        long[] sum = new long[candiesCount.length + 1];
+        for (int i = 1; i <= candiesCount.length; i++) {
+            sum[i] = sum[i - 1] + candiesCount[i - 1];
+        }
+        boolean[] result = new boolean[queries.length];
+        for (int i = 0; i < queries.length; i++) {
+            int t = queries[i][0];
+            int d = queries[i][1] + 1;
+            int c = queries[i][2];
+            long fast = sum[t] / c + 1;
+            long slow = sum[t + 1];
+            result[i] = fast <= d && d <= slow;
+        }
+        return result;
+    }
+}
