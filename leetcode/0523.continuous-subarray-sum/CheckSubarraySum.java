103e79e2326c1cc6941337020558cdfb4f123b7f
changqingyeh <yecq1998@outlook.com>
2021-06-02 22:50:47 +0800

    523. 连续的子数组和

---
+++ leetcode/0523.continuous-subarray-sum/CheckSubarraySum.java
@@ -0,0 +1,20 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class CheckSubarraySum {
+
+    public boolean checkSubarraySum(int[] nums, int k) {
+        int[] sum = new int[nums.length + 1];
+        for(int i = 1; i <= nums.length; i++){
+            sum[i] = sum[i - 1] + nums[i - 1];
+        }
+        Set<Integer> set = new HashSet<>();
+        for(int i = 2; i <= nums.length; i++){
+            set.add(sum[i - 2] % k);
+            if(set.contains(sum[i - 1] % k)){
+                return true;
+            }
+        }
+        return false;
+    }
+}
